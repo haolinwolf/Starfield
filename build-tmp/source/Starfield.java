39865468d4f94d0451939406100dadb569111ca7
haolinwolf <pinggal100@gmail.com>
2014-10-21 11:37:13 -0700

    Added Jumbo Particle to Starfield.ped

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -20,37 +20,43 @@ public void setup()
 {
 	size(400,400);//your code here
 	explosion = new Particle[200];
-	for (int nI = 0; nI < 199; nI++)
+	for (int nI = 0; nI < 198; nI++)
 	{
 		explosion[nI] = new NormalParticle();
+		explosion[198] = new JumboParticle();
 	}
+	
 	explosion[199] = new OddballParticle();
 	
 }
 public void draw()
 {
 	background(0);
-	for (int nI = 0; nI < 200; nI++)
+	for (int nI = 0; nI < 198; nI++)
 	{
 		explosion[nI].move();
 		explosion[nI].show();
+  
 	}
-
+	explosion[198].move();
+  explosion[198].show();
+ explosion[199].move();
 	explosion[199].show();//your code here
 }
 class NormalParticle implements Particle
 {
 	double myX, myY, speed, angle;
-	int colorA, colorB, colorC;
+	int colorA, colorB, colorC,mySize;
 	NormalParticle()
 	{
-	     myX = myX +(Math.random()*400);
-	     myY = myY +(Math.random()*400);
-	     speed = Math.random()*10;
+	     myX = 200;
+	     myY = 200;
+	     speed = Math.random()*5;
 	     angle = Math.PI*2*Math.random();
 	     colorA = (int)(Math.random()*251);
 	     colorB = (int)(Math.random()*251);  
 	     colorC = (int)(Math.random()*251); 
+	     mySize = 10;
 	}
 
 	public void move()
@@ -63,7 +69,7 @@ class NormalParticle implements Particle
 	public void show()
 	{
 	 fill(colorA,colorB,colorC,150);
-	 ellipse((int)myX,(int)myY,10,10);//your code here
+	 ellipse((int)myX,(int)myY,mySize,mySize);//your code here
 	}
 }
 interface Particle
@@ -71,14 +77,24 @@ interface Particle
 	public void show();
 	public void move();//your code here
 }
+class JumboParticle extends NormalParticle
+{
+  JumboParticle()
+  {
+  	mySize = 40;
+  	colorA = 150;
+  	colorB = 150;
+  	colorC = 150;
+  }
+}
 class OddballParticle implements Particle
 {
 	double myX, myY, angle, speed;
 	int myColor;
 	OddballParticle()
 	{
-	 myX = (Math.random()*400);
-	 myY = (Math.random()*400);
+	 myX = 200;
+	 myY = 200;
 	 angle = Math.PI*2*Math.random();
 	 speed = Math.random()*10; 
 	 myColor = 250;
